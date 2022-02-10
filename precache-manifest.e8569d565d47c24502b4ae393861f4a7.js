e4b102c4c995274a27df763fec660540954efe0b
zekpause <samson6603@gmail.com>
2022-02-10 17:07:05 +0800

    Updates

---
+++ precache-manifest.e8569d565d47c24502b4ae393861f4a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d97e3580c9edecef74772315688dd80",
+    "url": "/robotsfriend-pwa/index.html"
+  },
+  {
+    "revision": "40f76be43c3903d00730",
+    "url": "/robotsfriend-pwa/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "f83dd807af091a356a8e",
+    "url": "/robotsfriend-pwa/static/css/main.7dc2cbe0.chunk.css"
+  },
+  {
+    "revision": "40f76be43c3903d00730",
+    "url": "/robotsfriend-pwa/static/js/2.8dcd9cb9.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/robotsfriend-pwa/static/js/2.8dcd9cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83dd807af091a356a8e",
+    "url": "/robotsfriend-pwa/static/js/main.16d094e6.chunk.js"
+  },
+  {
+    "revision": "ebd1dc4c1f7d6a19f59a",
+    "url": "/robotsfriend-pwa/static/js/runtime-main.053c965c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotsfriend-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
